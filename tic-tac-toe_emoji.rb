9605186c26be1ff689e6982c7ada944831a51a8d
Amy MacKinnon <amy@amy-mac.com>
2013-09-29 13:28:02 -0700

    Updated the emoji version to match the original

--- tic-tac-toe_emoji.rb
+++ tic-tac-toe_emoji.rb
@@ -1,3 +1,9 @@
+class IllegalMove < StandardError
+  def message
+    "That space has already been played."
+  end
+end
+
 class Board
   
   attr_accessor :line1, :line2, :line3, :won
@@ -10,56 +16,50 @@ class Board
   end
   
   def display
-    puts " #{@line1[0]} | #{@line1[1]} | #{@line1[2]}"
-    puts "-----------"
-    puts " #{@line2[0]} | #{@line2[1]} | #{@line2[2]}"
-    puts "-----------"
-    puts " #{@line3[0]} | #{@line3[1]} | #{@line3[2]}"
+    line_width = 60
+    puts "  #{@line1[0]} | #{@line1[1]} | #{@line1[2]}  ".center(60)
+    puts "-------------".center(60)
+    puts "  #{@line2[0]} | #{@line2[1]} | #{@line2[2]}  ".center(60)
+    puts "-------------".center(60)
+    puts "  #{@line3[0]} | #{@line3[1]} | #{@line3[2]}  ".center(60)
+  end
+  
+  def illegal_move
+    
   end
   
   def winning
     if (@line1 || @line2 || @line3) == (['❌', '❌', '❌'] || ['⭕', '⭕', '⭕'])
-      puts "You won!"
       @won = true
     end
     if (@line1[0] == '❌') && (@line2[0] == '❌') && (@line3[0] == '❌')
-      puts 'You won!'
       @won = true
     end
     if (@line1[0] == '⭕') && (@line2[0] == '⭕') && (@line3[0] == '⭕')
-      puts 'You won!'
       @won = true
     end
     if (@line1[1] == '❌') && (@line2[1] == '❌') && (@line3[1] == '❌') 
-      puts 'You won!'
       @won = true
     end
     if (@line1[1] == '⭕') && (@line2[1] == '⭕') && (@line3[1] == '⭕')
-      puts 'You won!'
       @won = true
     end
     if (@line1[2] == '❌') && (@line2[2] == '❌') && (@line3[2] == '❌')
-      puts 'You won!'
       @won = true
     end
     if (@line1[2] == '⭕') && (@line2[2] == '⭕') && (@line3[2] == '⭕')
-      puts 'You won!'
       @won = true
     end
     if (@line1[0] == '❌') && (@line2[1] == '❌') && (@line3[2] == '❌') 
-      puts 'You won!'
       @won = true
     end
     if (@line1[0] == '⭕') && (@line2[1] == '⭕') && (@line3[2] == '⭕')
-      puts 'You won!'
       @won = true
     end
     if (@line1[2] == '❌') && (@line2[1] == '❌') && (@line3[0] == '❌') 
-      puts 'You won!'
       @won = true
     end
     if (@line1[2] == '⭕') && (@line2[1] == '⭕') && (@line3[0] == '⭕')
-      puts 'You won!'
       @won = true
     end
   end
@@ -88,56 +88,123 @@ end
 def new_game
   puts "What's Player 1's name?"
   player1 = gets.chomp
+  puts ''
   puts "What's Player 2's name?"
   player2 = gets.chomp
+  puts ''
   puts "Here is your game board:"
   puts ''
+  
+  if player1.empty?
+    player1 = 'Player 1'
+  elsif player2.empty?
+    player2 = 'Player 2'
+  end
   
   players = Players.new(player1, player2)
   play_game = Board.new
   turn = 0
   
   while play_game.won == false
+    puts `clear`
     play_game.display
+    puts ''
+    
     if turn.even?
       current_player_sign = players.player1_sign
       current_player_name = players.player1
+    
     else
       current_player_sign = players.player2_sign
       current_player_name = players.player2
     end
     
-    puts "#{current_player_name} select your number"
-    answer = gets.chomp.to_i
+    begin
+      puts "#{current_player_name}, select your space:"
+      answer = gets.chomp.to_i
+    
+      unless answer.zero?
   
-    case answer
-    when 1
-      play_game.line1[0] = current_player_sign
-    when 2
-      play_game.line1[1] = current_player_sign
-    when 3
-      play_game.line1[2] = current_player_sign
-    when 4
-      play_game.line2[0] = current_player_sign
-    when 5
-      play_game.line2[1] = current_player_sign
-    when 6
-      play_game.line2[2] = current_player_sign
-    when 7
-      play_game.line3[0] = current_player_sign
-    when 8
-      play_game.line3[1] = current_player_sign
-    when 9
-      play_game.line3[2] = current_player_sign
+        case answer
+        when 1
+          if play_game.line1[0].is_a? Integer
+            play_game.line1[0] = current_player_sign
+          else
+            raise IllegalMove
+          end
+        when 2
+          if play_game.line1[1].is_a? Integer
+            play_game.line1[1] = current_player_sign 
+          else
+            raise IllegalMove
+          end
+        when 3
+          if play_game.line1[2].is_a? Integer
+            play_game.line1[2] = current_player_sign
+          else
+            raise IllegalMove
+          end
+        when 4
+          if play_game.line2[0].is_a? Integer
+            play_game.line2[0] = current_player_sign
+          else
+            raise IllegalMove
+          end
+        when 5
+          if play_game.line2[1].is_a? Integer
+            play_game.line2[1] = current_player_sign
+          else
+            raise IllegalMove
+          end
+        when 6
+          if play_game.line2[2].is_a? Integer
+            play_game.line2[2] = current_player_sign
+          else
+            raise IllegalMove
+          end
+        when 7
+          if play_game.line3[0].is_a? Integer
+            play_game.line3[0] = current_player_sign
+          else
+            raise IllegalMove
+          end
+        when 8
+          if play_game.line3[1].is_a? Integer
+            play_game.line3[1] = current_player_sign
+          else
+            raise IllegalMove
+          end
+        when 9
+          if play_game.line3[2].is_a? Integer
+            play_game.line3[2] = current_player_sign
+          else
+            raise IllegalMove
+          end
+        end
+      end
+    
+    rescue IllegalMove => e
+      puts e.message
+      retry
     end
     
     turn += 1
+    
     if turn == 9
       puts "It's a tie!"
+      play_game.display
+      puts "'The only winning move is not to play.' -War Games"
       Process.exit(0)
+    
     else
       play_game.winning
+      if play_game.won == true
+        puts "Congratulations #{current_player_name}, you won!"
+        puts ''
+        play_game.display
+      end
     end
+  
   end
     
 end
